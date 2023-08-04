4bb124cd5b8ec3c2a027c47cc9c38ebed32c33f8
Umnov Stanislav <umnov@cmd.su>
2023-08-04 11:46:34 +0300

    DI + Repository

---
+++ GameIndustry/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameIndustry.Models;
+
+namespace GameIndustry.Repository
+{
+    interface IRepository
+    {
+        Task<List<Game>> GetAll();
+    }
+}
