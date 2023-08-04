12f1d77374735c70d0b4bb53154124da86b5100a
Umnov Stanislav <umnov@cmd.su>
2023-08-04 16:27:15 +0300

    Add modal window for confirmation of deleting a game

---
+++ GameIndustry/Pages/Counter.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameIndustry.Pages
+{
+    public partial class Counter
+    {
+        [Inject] SingletonService single { get; set; }
+        [Inject] TransientService transient { get; set; }
+        private int currentCount = 0;
+        private void IncrementCount()
+        {
+            currentCount++;
+            single.Value++;
+            transient.Value++;
+        }
+    }
+}
